3b41597bb818168bc1f8d0b5037483df0280e51a
jasonma2016 <yecheng_ma@college.harvard.edu>
2018-12-08 19:53:27 -0500

    almost done

--- BF.py
+++ BF.py
@@ -1,52 +1,29 @@
 import numpy as np
 import torch
-from process import *
 from filters.BloomFilter import BloomFilter
 import sys
 import math
 from helpers import *
 
-def run(FPR, alpha=0.6185):
-	# get the data
-	train_list, test_list, S_train, S_test, S_test_adv = process_shirt()
-	BF = BloomFilter(len(S_train), FPR)
-	for x in S_train:
-		BF.add(x)
-	print("Bloom Filter size:", BF.size / 8)
-	print("")
-	
-	fps = 0
-	total = 0
-	fps_adv = 0
-	total_adv = 0
-	
-	for x in test_list:
-		x_str = makestring(list(x[0].numpy().flatten()))
-		if x_str in S_train:
-			continue
-		total += 1
-		adv = False
-		fp = False
-		if (x[1] == 1):
-			total_adv += 1
-			adv = True
-		result = BF.check(x_str)
-		if result == True:
-			fps += 1
-			fp = True
-		if adv and fp:
-			fps_adv += 1
+def run_BF(FPR):
+  BF = BloomFilter(len(phishing_URLs), FPR)
+  for url in phishing_URLs:
+    BF.add(url)
+  
+  
+  fps = 0
+  total = 0
+  total_time = 0
+  for url in testing_list:
+    total += 1
+    start = time.time()
+    result = BF.check(url)
+    end = time.time()
+    total_time += (end-start)
+    if result == True:
+      fps += 1
+  
+  avg_fp = fps/total
 
-	avg_fp = fps/total
-	avg_fp_adv = fps_adv/total_adv
-
-	print("non adversarial (all test images)")
-	print("fps:", fps, "total:", total)
-	print("avg:", avg_fp)
-	print("="*30)
-	print("adversarial (all test shirts)")
-	print("fps:", fps_adv, "total:", total_adv)
-	print("avg:", avg_fp_adv)
-
-if __name__ == "__main__":
-	run(float(sys.argv[1]))
+  # returns empirical FPR, BF size in bytes, and access time per element
+  return avg_fp, BF.size/8, (total_time)/len(testing_list)
--- README.md
+++ README.md
@@ -7,19 +7,3 @@ CS222 final project
 	- (Mac) conda install pytorch torchvision -c pytorch
 	- (Windows) conda install pytorch -c pytorch; pip3 install torchvision
 
-## Brief Overview
-/filters contains the basic Bloom Filter and learning model (CNN) implementations.
-
-All tests currently run on the shirts data set.
-
-BF.py tests a basic BF model given a desired FPR rate to be achieved.
-example command: python3 BF.py 0.01
-
-LBF.py tests a LBF model given a desired FPR rate to be achieved (data set dependent) and tau. 
-example command: python3 LBF.py 0.01 0.5
-
-SBF.py tests a SBF model given a deisred FPR rate to be achieved (data set dependent) and tau.
-example command: python3 SBF.py 0.005 0.5
-
-SBF_tau.py tests the same learning model + SBF with different cutoff value tau.
-example command: python3 SBF_tau.py 0.005
